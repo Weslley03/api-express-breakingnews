859347dedeebb873235651f885e0f8c37bc216af
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-05-14 16:03:48 -0300

    integrado criação de user com banco

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,22 @@
-const create = (req, res) => {
+const userService = require('../services/user.service')
+
+const create = async (req, res) => {
     const { name,userName,email,password,avatar,background } = req.body
 
     if(!name || !userName || !email || !password || ! avatar || !background) { 
         res.status(400).send({message: "existem campos faltantes"})
     }
 
+    const user  = await userService.create(req.body)
+    if(!user) {
+        return res.status(400).send({message: 'ERROR CREATE USER'})
+    }
+
     res.status(201).send({
         menssage: "user create succesfully",
 
         user: {
+            id: user._id,
             name,
             userName,
             email,
--- src/database/db.js
+++ src/database/db.js
@@ -5,7 +5,7 @@ const connetcDatabase = () => {
 
   mongoose
     .connect(
-      "mongodb+srv://oweslley03:BancoMongo03Weslley @cluster0.b3uma5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://oweslley03:BancoMongo03Weslley@cluster0.b3uma5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => console.log("mongoDB Atlas conectado"))
     .catch((err) => console.log(err));
