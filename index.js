8aee34d617ad1a9ee92b7ec1288a1874ed7d6590
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-05-14 15:45:59 -0300

    banco conectado

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const app = express()
 const userRoute = require('./src/routes/user.route')
+const connetcDatabase = require('./src/database/db')
+
+const app = express()
 const port = 3000
+connetcDatabase()
 
 app.use(express.json())
 app.use('/users', userRoute)
---
+++ src/database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connetcDatabase = () => {
+  console.log("conectando.. ");
+
+  mongoose
+    .connect(
+      "mongodb+srv://oweslley03:BancoMongo03Weslley @cluster0.b3uma5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => console.log("mongoDB Atlas conectado"))
+    .catch((err) => console.log(err));
+}
+
+module.exports = connetcDatabase 
