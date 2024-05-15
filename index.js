721e17f0a9914af0e3039885a1b94069c9dc030e
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-05-15 17:23:01 -0300

    implementado .env/dotenv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import userRoute from './src/routes/user.route.js'
 import connetcDatabase from './src/database/db.js'
+import dotenv from "dotenv" //chama a blibioteca
+dotenv.config() //declara sua configuração para uso
 
 const app = express()
-const port = 3000
-connetcDatabase()
+const port = process.env.PORT || 3000
+connetcDatabase() //executa a função de conexão ao banco
 
 app.use(express.json())
 app.use('/users', userRoute)
--- src/database/db.js
+++ src/database/db.js
@@ -5,7 +5,7 @@ const connetcDatabase = () => {
 
   mongoose
     .connect(
-      "mongodb+srv://oweslley03:BancoMongo03Weslley@cluster0.b3uma5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b3uma5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     )
     .then(() => console.log("mongoDB Atlas conectado"))
     .catch((err) => console.log(err));
