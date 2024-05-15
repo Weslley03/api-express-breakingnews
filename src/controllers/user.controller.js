2f7b3abef1f88f03596867cafc9a234107732ca0
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-05-15 09:51:00 -0300

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,86 +1,94 @@
-const { json } = require('express')
-const userService = require('../services/user.service')
-const mongoose = require("mongoose")
+const { json } = require("express");
+const userService = require("../services/user.service");
+const mongoose = require("mongoose");
 
 const create = async (req, res) => {
-    const { name,userName,email,password,avatar,background } = req.body
+  const { name, userName, email, password, avatar, background } = req.body;
 
-    if(!name || !userName || !email || !password || ! avatar || !background) { 
-        res.status(400).send({message: "existem campos faltantes"})
-    }
+  if (!name || !userName || !email || !password || !avatar || !background) {
+    res.status(400).send({ message: "existem campos faltantes" });
+  }
 
-    const user = await userService.createService(req.body)
-    if(!user) {
-        return res.status(400).send({message: 'ERROR CREATE USER'})
-    }
+  const user = await userService.createService(req.body);
+  if (!user) {
+    return res.status(400).send({ message: "ERROR CREATE USER" });
+  }
 
-    res.status(201).send({
-        menssage: "user create succesfully",
+  res.status(201).send({
+    menssage: "user create succesfully",
 
-        user: {
-            id: user._id,
-            name,
-            userName,
-            email,
-            avatar,
-            background
-        }
-    })
-}
+    user: {
+      id: user._id,
+      name,
+      userName,
+      email,
+      avatar,
+      background,
+    },
+  });
+};
 
 const findAll = async (req, res) => {
-    const users = await userService.findAllService()
+  const users = await userService.findAllService();
 
-    if(!users) {
-        res.status(400).send({message: 'não existem usuarios para pesquisa'})
-    }
-    res.send(users)
-}
+  if (!users) {
+    res.status(400).send({ message: "não existem usuarios para pesquisa" });
+  }
+  res.send(users);
+};
 
 const findById = async (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        res.status(400).send({message: 'o ID tá certo, caralho?'})
-    } else {
-        const userPorId = await userService.GetByIdService(id)
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).send({ message: "o ID tá certo, caralho?" });
+  } else {
+    const userPorId = await userService.GetByIdService(id);
 
-    if(!userPorId) {
-        res.status(400).send({message: 'o ID tá certo, caralho?'})
+    if (!userPorId) {
+      res.status(400).send({ message: "o ID tá certo, caralho?" });
     }
-    res.send(userPorId)
-    }
-}
+    res.send(userPorId);
+  }
+};
 
 const update = async (req, res) => {
-    const { name,userName,email,password,avatar,background } = req.body
+  const { name, userName, email, password, avatar, background } = req.body;
 
-    if(!name && !userName && !email && !password && !avatar && !background) { 
-        res.status(400).send({message: "necessario alterar ao menos um campo"})
-    }
+  if (!name && !userName && !email && !password && !avatar && !background) {
+    res.status(400).send({ message: "necessario alterar ao menos um campo" });
+  }
 
-    const id = req.params.id
-    let userExiste = await userService.GetByIdService(id)
-    if(!userExiste){
-        res.status(400).send({message: 'esse usuario não existe !!'})
-    } else {
-        userExiste = await userService.updateService(id, req.body)
-        res.status(200).send({message: 'usuario alterado com sucesso'})
-    }
-}
+  const id = req.params.id;
+  let userExiste = await userService.GetByIdService(id);
+  if (!userExiste) {
+    res.status(400).send({ message: "esse usuario não existe !!" });
+  } else {
+    userExiste = await userService.updateService(
+      id,
+      name,
+      userName,
+      email,
+      password,
+      avatar,
+      background
+    );
+    res.status(200).send({ message: "usuario alterado com sucesso" });
+  }
+};
 
 const remove = async (req, res) => {
-    const id = req.params.id
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        res.status(400).send({message: 'o ID tá certo, caralho?'})
-    } 
-    let userPorId = await userService.GetByIdService(id)
-    if(!userPorId) {
-        res.status(400).send({message: 'digita essa porra certo'})
-    } else {
-        userPorId = await userService.deleteService(id)
-        res.status(200).send({message: 'usuario foi com Deus'})
-    }
-}
+  const id = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).send({ message: "o ID tá certo, caralho?" });
+  }
+  let userPorId = await userService.GetByIdService(id);
+  if (!userPorId) {
+    res.status(400).send({ message: "digita essa porra certo" });
+  } else {
+    userPorId = await userService.deleteService(id);
+    res.status(200).send({ message: "usuario foi com Deus" });
+  }
+};
 
-module.exports = { create, findAll, findById, update, remove}
+module.exports = { create, findAll, findById, update, remove };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,9 +1,28 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
-const createService = (body) => User.create(body)
-const findAllService = () => User.find()
-const GetByIdService = (id) => User.findById(id)
-const updateService = (id, body) => User.findByIdAndUpdate(id, body)
-const deleteService = (id) => User.findByIdAndDelete(id)
+const createService = (body) => User.create(body);
+const findAllService = () => User.find();
+const GetByIdService = (id) => User.findById(id);
+const updateService = (
+  id,
+  name,
+  userName,
+  email,
+  password,
+  avatar,
+  background
+) =>
+  User.findByIdAndUpdate(
+    { _id: id },
+    { name, userName, email, password, avatar, background }
+  );
+  
+const deleteService = (id) => User.findByIdAndDelete(id);
 
-module.exports = { createService, findAllService, GetByIdService, updateService, deleteService }
+module.exports = {
+  createService,
+  findAllService,
+  GetByIdService,
+  updateService,
+  deleteService,
+};
