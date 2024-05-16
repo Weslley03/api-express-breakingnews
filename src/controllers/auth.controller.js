3fc7e14bf05b65f23d5f7a7ca96cecd74b378099
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-05-16 18:06:44 -0300

    TOKEN jwt generate

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { loginService } from "../services/auth.service.js";
+import { loginService, generateToken } from "../services/auth.service.js";
 
 const login = async (req, res) => {
   try {
@@ -16,7 +16,9 @@ const login = async (req, res) => {
       return res.status(400).send({ message: "usuario ou senha invalidos" });
     }
 
-    res.send('LOGIN OK');
+    const token = generateToken(user.id)
+
+    res.send({token});
   } catch (err) {
     res.status(500).send(`houve um erro: ${err}`);
   }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,14 @@
+import jwt from "jsonwebtoken"
 import User from "../models/User.js"
+import dotenv from "dotenv"
+dotenv.config()
 
 const loginService = (email) => {
     return User.findOne({email: email}).select('+password')
 }
 
-export { loginService }
+const generateToken = (id) => {
+    return jwt.sign({id: id}, process.env.SECRET_JWT, {expiresIn: 86400})
+}
+
+export { loginService, generateToken }
