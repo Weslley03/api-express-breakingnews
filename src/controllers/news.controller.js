689080f9efebcc9fcff2becbe90f946a445fa1e8
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-05-17 15:24:18 -0300

    findAll news OK

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -2,27 +2,34 @@ import { createService, findAllService } from "../services/news.service.js";
 
 export const create = async (req, res) => {
   try {
-    const { title, text, banner } =  req.body
+    const { title, text, banner } = req.body;
 
-    if(!title || !text || !banner) {
-        res.status(500).send({message: "existem dados faltantes"});
+    if (!title || !text || !banner) {
+      res.status(500).send({ message: "existem dados faltantes" });
     }
     await createService({
-        title,
-        text, 
-        banner,
-        id: 'objectIdFake'
-    })
+      title,
+      text,
+      banner,
+      id: "objectIdFake",
+    });
 
     res.send("criação de post");
-    
   } catch (err) {
     res.status(500).send({ message: err.message });
-    console.log('foi pro erro')
+    console.log("foi pro erro");
   }
 };
 
 export const findAll = async (req, res) => {
-  const news = [];
-  res.send(news);
+  try {
+    const news = await findAllService();
+    if(news.length === 0) {
+        res.status(404).send({ message: 'não tem noticia véi' });
+    }
+    res.send(news);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+    console.log("foi pro erro");
+  }
 };
