6d8cf0d731e4c6c4df2d72524a168b33cb60a59a
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-05-20 16:40:52 -0300

    paginação com GET de NOTICIAS

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../services/news.service.js";
+import { createService, findAllService, countNews } from "../services/news.service.js";
 
 export const create = async (req, res) => {
   try {
@@ -23,11 +23,48 @@ export const create = async (req, res) => {
 
 export const findAll = async (req, res) => {
   try {
-    const news = await findAllService();
+    let { limit, offset } = req.query
+    limit = Number(limit)
+    offset = Number(offset) //onde eu começo 'skip'
+
+    if(!limit) {
+      limit = 5
+    }
+    if(!offset) {
+      offset = 0
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews()
+    const currentUrl = req.baseUrl
+    
+    const next = offset + limit
+    const nextUrl = next < total ? `${currentUrl}?limi=${limit}?offset=${next}` : null
+    
+    const previous = offset * limit < 0 ? null : offset - limit
+    const previousUrl = previous != null ? `${currentUrl}?limi=${limit}?offset=${previous}` : null
+
     if(news.length === 0) {
         res.status(404).send({ message: 'não tem noticia véi' });
     }
-    res.send(news);
+    res.send({
+      nextUrl, 
+      previousUrl,
+      limit,
+      offset,
+      total,
+      results: news.map((newItem) => ({
+        id: newItem._id,
+        title: newItem.title,
+        text: newItem.text,
+        banner: newItem.banner,
+        likes: newItem.likes,
+        comments: newItem.comments,
+        name: newItem.user.name,
+        userName: newItem.user.username,
+        userAvatar: newItem.user.avatar
+      }))
+    });
   } catch (err) {
     res.status(500).send({ message: err.message });
     console.log("foi pro erro");
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -1,5 +1,13 @@
 import News from "../models/News.js"
 
+export const countNews = () => News.countDocuments()
 export const createService = (body) => News.create(body); 
-export const findAllService = () => News.find();
+export const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate('user')
+/*xxxxx.find()
+.sort({_id: -1}) nesse caso, trás os dados de trás pra frente, conforme criação dos ID (_id)
+.skip(offset) de quantos em quantos o banco irá pular par trazer
+.limit(limit) quantidade de amostra
+.populate('user') através do ID, busca o USER e mostra também na busca 'relacionamento de tabela'
+*/
+
 
