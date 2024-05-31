80f70354f7b2712824bb127e0f82ebb574eee888
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-05-31 14:02:03 -0300

    cors aply

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import connetcDatabase from './src/database/db.js'
 import dotenv from "dotenv" //chama a blibioteca
+import cors from 'cors'
 
 //import de toda rota do programa
 import userRoute from './src/routes/user.route.js'
@@ -9,6 +10,7 @@ import newsRoute from './src/routes/news.route.js'
 import swaggerRoute from './src/routes/swagger.route.js'
 
 const app = express()
+app.use(cors())
 dotenv.config()
 const port = process.env.PORT || 3000
     
