ace9ee214962df23de8b3fe5e9b8ebd92bf6e144
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-06-25 15:57:03 -0300

    mensagem especifica de erro, LOGIN

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,26 +1,19 @@
-import bcrypt from "bcrypt";
 import { loginService, generateToken } from "../services/auth.service.js";
 
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await loginService(email);
+    const { user, ok, message } = await loginService(email, password);
 
-    if (!user) {
-      return res.status(400).send({ message: "usuario ou senha invalidos" });
-    }
-
-    const passwordIsValid = bcrypt.compareSync(password, user.password);
-
-    if (!passwordIsValid) {
-      return res.status(400).send({ message: "usuario ou senha invalidos" });
+    if (!user || ok === false) {
+      return res.status(400).json({ message });
     }
 
     const token = generateToken(user.id)
-
-    res.send({token});
+    return res.send({token});
   } catch (err) {
     res.status(500).send(`houve um erro: ${err}`);
   }
 };
+
 export { login };
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,10 +1,26 @@
 import jwt from "jsonwebtoken"
 import User from "../models/User.js"
+import bcrypt from "bcrypt";
 import dotenv from "dotenv"
 dotenv.config()
 
-const loginService = (email) => {
-    return User.findOne({email: email}).select('+password')
+const loginService = (email, password) => {
+
+    try{
+        const user = User.findOne({email: email}).select('+password')
+        if(!user){
+            return { ok: false, message: 'não foi executar o service de LOGIN' }
+        }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if (!passwordIsValid) {
+          return { ok: false, message: "usuario ou senha invalidos" }
+        }
+
+        return {ok: true, user}
+    }catch(err){
+        console.log('houve um erro no service do back-end', err)
+    }
 }
 
 const generateToken = (id) => {
