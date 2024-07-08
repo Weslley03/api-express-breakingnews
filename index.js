8d0fec9e21a666fc75f31515b1181a966759f93f
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-07-08 16:35:54 -0300

    edit bodyParser for 50mb support

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 
 const app = express()
-app.use(bodyParser.json({ limit: '100mb' }))
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
 //import de toda rota do programa
 import userRoute from './src/routes/user.route.js'
